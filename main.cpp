3775aa2196a29895bae43c86fe01da3a543a56db
Shivendra TechSter <shivendratechster@gmail.com>
2023-02-12 00:06:20 +0530

    update usage example in main

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 
 int main()
 {
-    vector numbers(10);
+    vector numbers(5);
 
-    numbers.push_back(10.5);
-    numbers.push_back(19.13);
-    numbers.push_back(210.59);
-    numbers.push_back(0.35);
-    numbers.push_back(100.00);
+    numbers[0] = 32.45;
+    numbers[1] = 312.345;
+    numbers[2] = 532.545;
+    numbers[3] = 932.145;
+    numbers[4] = 55.45;
 
-    std::cout << numbers[2] << std::endl;
+    numbers.push_back(729.123);
+
+    std::cout << numbers[5] << std::endl;
 
     return 0;
 }
